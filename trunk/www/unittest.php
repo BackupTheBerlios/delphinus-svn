d0ef50baf3740f85d87a07e0a149da7d60ddd761
ha1t <ha1t@736708dc-3d03-0410-9be7-fec9261d445c>
2006-05-15 10:32:34 +0000

    add entrypoint

---
+++ trunk/www/unittest.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(E_ALL);
+include_once '/home/halt/codes/delphinus/app/Delphinus_Controller.php';
+
+Delphinus_Controller::main('Delphinus_Controller', array(
+    '__ethna_unittest__',
+    )
+);
+?>
---
+++ trunk/www/xmlrpc.php
@@ -0,0 +1,5 @@
+<?php
+include_once('/home/halt/codes/delphinus/app/Delphinus_Controller.php');
+
+Delphinus_Controller::main_XMLRPC('Delphinus_Controller');
+?>
